ff9722049149e9f0d8ec0359895dada71e7c94c0
Aggrimma Sharma <aggrimma09@gmail.com>
2024-06-15 23:18:54 +0530

    Exercise_And_Ecommerce_Components_Added

---
+++ src/components/Exercises/Exercises.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Exercises.css'
+function Exercises() {
+  return (
+    <div>
+    <div className='fs-2 fw-bold'>Exercises to follow </div>
+    <img src='https://cdn.dribbble.com/users/5501300/screenshots/15456745/media/ebfa85d44e6577c89166446d06840c28.gif' width="500px" height="500px"></img>
+    <img src='https://cdnb.artstation.com/p/assets/images/images/037/461/437/original/digital-artist-leg-pull-in.gif?1620422767' width="500px" height="500px"></img>
+    <img src='https://www.kibrispdr.org/data/1761/exercise-gif-31.jpg' width="500px" height="500px"></img>
+    <img src='https://cutewallpaper.org/24/free-gif-animation/artstation-exercise-fitness-vector-gif-animation-line-illustration-%E9%87%91%E6%99%A8.gif' width="500px" height="500px"></img>
+
+    </div>
+  )
+}
+
+export default Exercises
