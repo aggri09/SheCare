93d80d96c02637fbf8771d85a3778908c2fad72e
Gauri <gauri.gandhii2004@gmail.com>
2024-06-18 15:01:15 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutUs from './Components/AboutUs/AboutUs'
+import RootLayout from './RootLayout';
+import Health from './Components/Health/Health'
+import Main from './Components/Main/Main'
+import Fitness from './Components/Fitness/Fitness'
+import MenstrualTrack from './Components/MenstrualTrack/MenstrualTrack'
+import Faqs from './Components/FAQS/Faqs'
+import Ecommerce from './Components/Ecommerce/Ecommerce';
+import PregnancyDiet from './Components/PregnancyDiet/PregnancyDiet';
+import NutritionPlans from './Components/NutritionPlans/NutritionPlans';
+import UnderWeight from './Components/UnderWeight/UnderWeight';
+import RegularDiet from './Components/RegularDiet/RegularDiet';
+import ExtremeStrict from './Components/ExtremeStrict/ExtremeStrict';
+import Exercises from './Components/Exercises/Exercises';
 function App() {
+  //create browser router object
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<RootLayout/>,
+      children:[
+        {
+          path:"/",
+          element:<Main/>
+        },
+        {
+          path:"/aboutus",
+          element:<AboutUs/>
+      },
+      {
+        path:"/health",
+        element:<Health/>,
+        children:[
+          {
+            path:"menstrual",
+            element:<MenstrualTrack/>
+          },
+          {
+            path:"fitness",
+            element:<Fitness/>,
+          },
+          {
+            path:"nutritionplans",
+            element:<NutritionPlans/>,
+            children:[
+              {
+              path:"underWeight",
+              element:<UnderWeight/>
+              },
+              {
+                path:"regularDiet",
+                element:<RegularDiet/>
+              },
+              {
+                path:"extremeStrict",
+                element:<ExtremeStrict/>
+              },
+              {
+                path:"exercise",
+                element:<Exercises/>
+              }
+            ]
+          },
+          {
+            path:"faqs",
+            element:<Faqs/>
+          },
+          {
+            path:"pregnancy",
+            element:<PregnancyDiet/>
+          }
+        ]
+      },
+      {
+        path:"/ecommerce",
+        element:<Ecommerce/>
+      }
+    ]
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <RouterProvider router={router}/>
     </div>
   );
 }
